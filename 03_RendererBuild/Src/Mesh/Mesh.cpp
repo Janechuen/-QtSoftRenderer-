5cac87fbd7a4ff7ce8aac14b261eda955b4e2fc2
cy <bobolili2world@gmail.com>
2023-04-24 18:56:42 +0800

    Mesh类更新OBJ模型加载功能

--- 03_RendererBuild/Src/Mesh/Mesh.cpp
+++ 03_RendererBuild/Src/Mesh/Mesh.cpp
@@ -50,15 +50,62 @@ void Mesh::LoadMesh(const std::string filename)
 	{
 		std::getline(in, line);
 		std::istringstream iss(line.c_str());
-		char trash;
-		if (!line.compare(0,2,"v "))
+		char trash;//trash 是一个临时变量，它被用作占位符，用于丢弃输入流中的字符
+		if (!line.compare(0,2,"v "))//测试含有v字符为顶点位置信息
 		{
+			/*
+			顶点数据格式
+			v -0.500000 -0.500000 0.500000
+			*/
 			iss >> trash;
 			vec3 v;
 			for(int i = 0; i < 3; i++)iss >> v[i];
 			position.push_back(v);//存储顶点
 		}
-		
+		else if(!line.compare(0,3,"vn "))//测试含有vn字符为顶点法线信息
+		{
+			/*
+			顶点法线数据格式
+			vn 0.000000 0.000000 1.000000
+			*/
+			iss >> trash >> trash;
+			vec3 n;
+			for (int i = 0; i < 3; i++)iss >> n[i];
+			normal.push_back(n);
+		}
+		else if(!line.compare(0,3, "vt "))//测试含有vt字符为顶点uv信息
+		{
+			/*
+			顶点法线数据格式
+			vt 0.375000 0.000000
+			*/
+			iss >> trash >> trash;// iss>> 跳过字符 这样可以跳过不需要的字符，只提取有用的数据，并将其存储到相应的数据结构中
+			vec2 uv;
+			for (int i = 0; i < 2; i++)iss >> uv[i];
+			texcoord.push_back({ uv.x, 1 - uv.y });
+		}
+		else if(!line.compare(0,2,"f "))//测试含有f字符为顶点面信息
+		{
+			/*
+			面数据格式（顶点 UV 法线 的索引）
+			f 1/1/1 2/2/2 4/4/3 3/3/4
+			*/
+			int f, t, n;
+			iss >> trash;
+			int cnt;
+			while (iss>>f>>trash>>t>>trash>>n)//面数据的顶点索引、纹理坐标索引和法线索引依次读取并存储到变量
+			{
+				vertexindex.push_back(f);//位置索引
+				textureindex.push_back(t);//纹理索引
+				normalindex.push_back(n);//法线索引
+				cnt++;
+			}
+			if (3!=cnt)
+			{
+				std::cerr << "ERROR : 模型不支持超过三角面的模型 " << std::endl;
+				return;
+			}
+		}
 	}
 	return;
 }
--- 03_RendererBuild/Src/Mesh/Mesh.h
+++ 03_RendererBuild/Src/Mesh/Mesh.h
@@ -3,7 +3,6 @@
 #include <fstream>//
 #include <sstream>//读取文件用
 #include <string>
-
 #include <vector>
 #include <cassert>
 #include <cmath>
@@ -13,10 +12,15 @@
 class Mesh 
 {	
 public:
-	
 	std::vector<Vertex> vertices; //position 属性
 	std::vector<unsigned int> index; //index 索引属性
 	std::vector<vec3>position;
+	std::vector<vec3>normal;
+	std::vector<vec2>texcoord;
+	std::vector<unsigned int> vertexindex; 
+	std::vector<unsigned int> textureindex;
+	std::vector<unsigned int> normalindex; 
+
 	Mesh() {}
 	~Mesh() {}
 	Mesh(const Mesh& msh) :vertices(msh.vertices), index(msh.index) {}
@@ -25,4 +29,5 @@ public:
 	void setIndex(int* i, int count); //设置EBO索引序列
 	void triangle(vec4& v1, vec4& v2, vec4& v3);
 	void LoadMesh(const std::string filename);
+private:
 };
